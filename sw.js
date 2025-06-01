442134af1d78fa04d5806fd534561af29731c0d8
xNightWulf69 <65742919+xNightWulf69@users.noreply.github.com>
2025-06-01 17:07:11 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,7 @@
-self.addEventListener('install',e=>{self.skipWaiting()});
-self.addEventListener('activate',e=>{e.waitUntil(clients.claim())});
-self.addEventListener('notificationclick',e=>{e.notification.close();e.waitUntil(clients.openWindow('/CalendarApp/'))});
-self.addEventListener('push',e=>{if(e.data){const data=e.data.json();e.waitUntil(self.registration.showNotification(data.title,{body:data.body,icon:'/CalendarApp/android-chrome-192x192.png',badge:'/CalendarApp/android-chrome-192x192.png',tag:'task-reminder'}))}});
+self.addEventListener('install', e => self.skipWaiting());
+self.addEventListener('activate', e => e.waitUntil(clients.claim()));
+
+self.addEventListener('notificationclick', e => {
+  e.notification.close();
+  e.waitUntil(clients.openWindow('/CalendarApp/'));
+});
